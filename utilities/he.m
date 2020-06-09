853dee96be6078dca269798aa663c61cc702a527
Peter Polcz <ppolcz@gmail.com>
2020-06-09 23:22:38 +0200

    L2 local nonlin siker + Duffing + Coutinho2008 + geostac modell legepelese

--- utilities/he.m
+++ utilities/he.m
@@ -6,12 +6,19 @@ classdef he
 %  Author: Peter Polcz (ppolcz@gmail.com) 
 %  
 %  Created on 2019. September 01. (2019a)
+%  Major review on 2020. May 22. (2019b)
 %
 
 %%
 
 
 methods
+    
+    % 2020.05.22. (május 22, péntek), 00:25
+    function [He,O,I] = he
+        O = @(varargin) zeros(varargin{:});
+        I = @(varargin) eye(varargin{:});
+    end
     
     function varargout = subsref(~,S)
         varargout{1} = S(1).subs{1} + S(1).subs{1}';
